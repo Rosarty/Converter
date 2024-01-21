c0d45910468c6a194821318124c619b499f07864
Ростислав <ros.artym@gmail.com>
2024-01-21 11:38:16 +0200

    Add addition

--- src/app/Main01.java
+++ src/app/Main01.java
@@ -4,12 +4,24 @@ import java.util.Scanner;
 
 public class Main01 {
     static double miles;
-
+    static double action;
+    private static final double COEFFICIENT  = 1.61;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Converting miles to kilometers :");
         System.out.println("Miles :");
         miles = scanner.nextDouble();
-
+        action = converts(miles);
+        double output = output();
+        System.out.printf("Converting : " + "%.2f", output);
     }
+
+    private static double converts(double miles) {
+        return miles * COEFFICIENT;
+    }
+
+    private static double output() {
+        return action;
+    }
+
 }
