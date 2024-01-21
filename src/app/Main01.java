88284a8ce6a749890294b184210de171911ffbf7
Ростислав <ros.artym@gmail.com>
2024-01-21 11:35:58 +0200

    Initial commit

---
+++ src/app/Main01.java
@@ -0,0 +1,15 @@
+package app;
+
+import java.util.Scanner;
+
+public class Main01 {
+    static double miles;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Converting miles to kilometers :");
+        System.out.println("Miles :");
+        miles = scanner.nextDouble();
+
+    }
+}
