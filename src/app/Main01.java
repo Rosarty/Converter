e186d4e3dc2b8110cde660775dc42e236c0c86e7
Ростислав <ros.artym@gmail.com>
2024-01-21 11:52:43 +0200

    Change addition

--- src/app/Main01.java
+++ src/app/Main01.java
@@ -3,21 +3,22 @@ package app;
 import java.util.Scanner;
 
 public class Main01 {
-    static double miles;
+    static double kilometers;
     static double action;
-    private static final double COEFFICIENT  = 1.61;
+    private static final double COEFFICIENT = 0.62;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Converting kilometers to miles :");
         System.out.println("Kilometers :");
-        miles = scanner.nextDouble();
-        action = converts(miles);
+        kilometers = scanner.nextDouble();
+        action = converts(kilometers);
         double output = output();
         System.out.printf("Converting : " + "%.2f", output);
     }
 
-    private static double converts(double miles) {
-        return miles * COEFFICIENT;
+    private static double converts(double kilometers) {
+        return kilometers * COEFFICIENT;
     }
 
     private static double output() {
